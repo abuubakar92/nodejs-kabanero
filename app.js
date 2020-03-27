9c2464eed7da85ff2d4ae916214806c163ab8462
abuubakar92 <abubakarbaridwan@gmail.com>
2020-03-27 14:56:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Appsody! github with Tekton on Openshift");
+  res.send("Demo from Appsody! github with Tekton on Openshift");
 });
  
 module.exports.app = app;
